bad7c9864947ca7e609ad68ddb42bd97820965f7
Yash Mishra <yashmishra12@hotmail.com>
2020-08-06 20:31:25 +0530

    comments added explaining problems

--- numberValidator.js
+++ numberValidator.js
@@ -1,6 +1,7 @@
+// Srapped from: https://en.wikipedia.org/wiki/List_of_mobile_telephone_prefixes_by_country#cite_note-5
 var data = require('./csvjson.json')
 //124 ---> India
-console.log(data[5]); 
+console.log(data[300]); 
 
 const validator = (code, phone)=>{
 
@@ -11,10 +12,14 @@ const validator = (code, phone)=>{
                 return true
             }
         }
-        
     }
     return false
 }
-const res = validator(376, 8130291250)
-
+const res = validator(1, 8130291250)
 console.log("Result: ",res); 
+
+// A small gotcha ---> 
+// Citizens of country which share their code with 
+// other countries whose phone len is not available
+// can give any phone number. 
+//FIX ---> Use Country Name along with phone code
